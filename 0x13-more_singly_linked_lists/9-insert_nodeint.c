419736e01bd44bd67239f1b52230a3575873f046
joshybobo <jmawuton@gmail.com>
2023-08-29 08:10:38 -0700

    9-insert_nodeint.c is updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (j = 0; temp && j < idx; j++)
 	{
-		if (j == jdx - 1)
+		if (j == idx - 1)
 		{
 			new->next = temp->next;
 			temp->next = new;
