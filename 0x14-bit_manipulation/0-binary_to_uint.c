b966a7e4a80c5b2a5b79c95368c025158b38ccee
joshybobo <jmawuton@gmail.com>
2023-08-31 15:23:52 -0700

    Testing

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    int index;
-    unsigned int decimal_value = 0;
+	int index;
+	unsigned int decimal_value = 0;
 
-    if (!b)
-        return (0);
+	if (!b)
+		return (0);
 
-    for (index = 0; b[index]; index++)
-    {
-        if (b[index] < '0' || b[index] > '1')
-            return (0);
-        decimal_value = 2 * decimal_value + (b[index] - '0');
-    }
+	for (index = 0; b[index]; index++)
+	{
+		if (b[index] < '0' || b[index] > '1')
+			return (0);
+		decimal_value = 2 * decimal_value + (b[index] - '0');
+	}
 
-    return (decimal_value);
+	return (decimal_value);
 }
