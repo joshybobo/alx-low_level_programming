7f219d214566975b6f9189d91d6dce8d871b4997
joshybobo <jmawuton@gmail.com>
2023-08-31 15:12:42 -0700

    Restyled

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,51 +1,25 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string containing a binary number.
+ * binary_to_uint - Converts a binary string to an unsigned integer.
+ * @b: The binary string to be converted.
  *
- * Return: The converted number, or 0 if there's an error.
+ * Return: The converted unsigned integer.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+    int index;
+    unsigned int decimal_value = 0;
 
-	if (b == NULL)
-	{
-		return (0);
-	}
+    if (!b)
+        return (0);
 
-	while (*b)
-	{
-		char current = *b;
-		if (current != '0' && current != '1')
-		{
-			return (0);
-		}
-		result = result * 2 + (current - '0');
-		b++;
-	}
-	return (result);
-}
+    for (index = 0; b[index]; index++)
+    {
+        if (b[index] < '0' || b[index] > '1')
+            return (0);
+        decimal_value = 2 * decimal_value + (b[index] - '0');
+    }
 
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0 (Success).
- */
-int main(void)
-{
-	const char *binary_str1 = "1101";
-	const char *binary_str2 = "10102";
-	const char *binary_str3 = NULL;
-
-	unsigned int result1 = binary_to_uint(binary_str1);
-	unsigned int result2 = binary_to_uint(binary_str2);
-	unsigned int result3 = binary_to_uint(binary_str3);
-
-	printf("Result 1: %u\n", result1);
-	printf("Result 2: %u\n", result2);
-	printf("Result 3: %u\n", result3);
-
-	return (0);
+    return (decimal_value);
 }
