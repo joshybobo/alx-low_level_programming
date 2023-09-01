3d556dab7bd27bc59ccf3ef1c07e3b46b1d0c000
joshybobo <jmawuton@gmail.com>
2023-08-31 17:24:28 -0700

    new

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,14 +1,12 @@
-#ifdef Main_h
-#define Main_h
+#ifndef MAIN
+#define MAIN
 
-unsigned int binary_to_unit(const char *b);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int _atoi(const char *s);
-int _putchar(char c);
 int get_endianness(void);
 
 
