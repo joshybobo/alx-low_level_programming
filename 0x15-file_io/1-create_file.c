6d9eae32254133e4563f8c2c0de4474ea865fdab
joshybobo <jmawuton@gmail.com>
2023-09-05 07:28:01 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,39 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file and writes text to it
- * @filename: Name of the file to create
- * @text_content: NULL-terminated string to write to the file
+ * create_file - Creates a file and writes text_content to it
+ * @filename: The name of the file to create
+ * @text_content: The content to write to the file
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, len = 0, write_status;
+	int fd, len, written;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[len])
-            len++;
-    }
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 
-    if (len > 0)
-    {
-        write_status = write(fd, text_content, len);
-        if (write_status == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+	len = 0;
+	while (text_content[len])
+		len++;
 
-    close(fd);
-    return (1);
+	written = write(fd, text_content, len);
+	if (written == -1 || written != len)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
 }
