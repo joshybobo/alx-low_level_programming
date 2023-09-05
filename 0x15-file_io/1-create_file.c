b52e36cd850ebfed8cf7ccdd24a32ee846ed4425
joshybobo <jmawuton@gmail.com>
2023-09-05 07:40:11 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,14 +1,17 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * create_file - Creates a file and writes text_content to it
- * @filename: The name of the file to create
- * @text_content: The content to write to the file
- * Return: 1 on success, -1 on failure
+ * create_file - Creates a file and writes text_content to it.
+ * @filename: The name of the file to create or truncate.
+ * @text_content: The content to write to the file.
+ * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len, written;
+	int fd, len, bytes_written;
 
 	if (filename == NULL)
 		return (-1);
@@ -17,21 +20,17 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (1);
-	}
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
 
-	len = 0;
-	while (text_content[len])
-		len++;
-
-	written = write(fd, text_content, len);
-	if (written == -1 || written != len)
-	{
-		close(fd);
-		return (-1);
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1 || bytes_written != len)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
