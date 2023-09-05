b520747945cb116a2fddec9c53a1ee142d7567d4
joshybobo <jmawuton@gmail.com>
2023-09-05 06:52:53 -0700

    confirm

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
  * read_textfile - Reads text from a file and prints it to STDOUT.
@@ -9,15 +11,20 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd, w, t;
 	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+
 	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
 	t = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, t);
 
