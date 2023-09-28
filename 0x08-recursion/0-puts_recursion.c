96b0e0f854987fbc37b06a5d1a4b3fb5aa52a60b
Joshybobo <jmawuton@gmail.com>
2023-09-28 15:08:30 -0700

    Test recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/* Function to print a character */
+void print_character(char c)
+{
+	 _putchar(c);
+}
+
+/* Function to print a newline character */
+void print_newline(void)
+{
+	 _putchar('\n');
+}
+
+int main(void)
+{
+	print_newline();  /* Call the print_newline function to print a newline */
+	return (0);
+}
