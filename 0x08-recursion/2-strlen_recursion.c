4ab95f577565cd68e441a69c30e43abf8e016f77
Joshybobo <jmawuton@gmail.com>
2023-09-29 16:32:00 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Returns lenght of string
+ * @s;string to be measured
+ *
+ * Return;lenght of string
+ */
+int_strlen_recursion(char *s)
+	intlongit = 0;
+	 *s == \0
+	{
+	longit++;
+	1 + _strlen_recursion(s + 1);
+	}
+	'return' (longit);
+	'}'
