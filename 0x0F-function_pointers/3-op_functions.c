38036772148816ba3527129a088fea210a88dd89
Joshybobo <jmawuton@gmail.com>
2023-10-14 07:21:37 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -51,24 +51,6 @@ int op_div(int a, int b)
 	{
 	printf("Error\n");
 	exit(100);
-
+	}
 	return (a / b);
 }
-/**
- * op_div - calculates the modulo of two numbers.
- * @a: first number.
- * @b: second number.
- *
- * Return: remainder of the division.
- */
-int op_div(int a, int b)
-	{
-	if (b == 0)
-	{
-	printf("Error\n");
-	exit(100);
-	}
-
-	return (a / b);
-
-	}
